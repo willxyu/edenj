da840373317612f33d78ece4754cb90d96c0627f
willxyu <willxyu@users.noreply.github.com>
2018-04-29 18:36:56 +1000

    Alterations
    Still not commited.

---
+++ user/core/core.main.js
@@ -0,0 +1,124 @@
+var t = {}
+t.aliases = `
+ +alias +n core.duanathar
+  +p ^dn$
+  +do
+     var s  = ''
+         s += 'get wings from pack' + sp
+         s += 'wear wings' + sp
+         s += 'say duanathar' + sp
+         s += 'remove wings' + sp
+         s += 'put wings in pack' + sp
+     fsend(s)
+  -en
+
+ +alias +n core.embraceFlame
+  +p ^ef$
+  +do
+     var s  = ''
+         s += 'embrace flame' + sp
+     fsend(s)
+  -en
+
+ +alias +n core.follow
+  +p ^f[ ]+(\\w+)$
+  +do
+     var s  = ''
+         s += 'follow ' + matches[1] + sp
+     fsend(s)
+  -en
+
+ +alias +n core.try
+  +p ^try[ ]+(.*)$
+  +do
+     var s = ''
+     var t = [
+       'push',
+       'pull',
+       'touch',
+       'open',
+       'unlock',
+       'light',
+       'kick',
+       'twist',
+       'snap', ]
+     for (var i=0; i<t.length; i++) {
+       s += t[i] + ' ' + matches[1] + sp
+     }
+     bsend(s)
+  -en
+
+ +alias +n core.walkto
+  +p ^wt[ ]+(.*)$
+  +do
+     var s  = ''
+         s += 'walkto ' + matches[1] + sp
+     fsend(s)
+  -en
+`
+
+t.triggers = `
+ +trigger +n core.afflicted
+  +p ^You have been afflicted with (.*)\.$
+  +do
+     highlight(matches[1],'red')
+  -en
+ +trigger +n core.level-highlight
+  +p ^You are level .* and (.*)% of the way to the next level\.$
+  +do
+     highlight(matches[1],'rgba(75,151,223,1)')
+  -en
+ +trigger +n core.gags
+  +p ^You are not fallen or kneeling\.$
+  +p ^You are already wielding that\.$
+  +p ^Ahh, I am truly sorry, but I do not see anyone by that name here\.$
+  +p ^You cannot see that being here\.$
+  +p ^You detect nothing here by that name\.$
+  +p ^I do not recognise anything called that here\.$
+  +p ^Nothing can be seen here by that name\.$
+  +p ^What do you wish to examine?
+  +p ^Your queues are already empty\.$
+  +do
+     sub()
+  -en
+ +trigger +n core.gag-eqbal
+  +p ^(.*) was added to your balance queue.$
+  +p ^(.*) was added to your equilibrium queue.$
+  +do
+      highlight(matches[1],'rgba(155,100,100,1)')
+      // sub()
+  -en
+ +trigger +n core.shop-autoadd
+  +p ^[ ]+.*\\d+[ ]+.*[ ]+(\\d+)[ ]+(\\d+)gp$
+  +do
+     var quant = matches[1]
+     var price = matches[2]
+     var total = price * quant
+     tagOn('   ' + total + ' g')
+  -en
+ +trigger +n core.ships-autoadd
+  +p ^Fine[ ]+Provisions[ ]+(\\d+)[ ]+(\\d+)$
+  +p ^Good[ ]+Provisions[ ]+(\\d+)[ ]+(\\d+)$
+  +p ^Common[ ]+Provisions[ ]+(\\d+)[ ]+(\\d+)$
+  +p ^Token[ ]+Stores[ ]+(\\d+)[ ]+(\\d+)$
+  +do
+     var price = matches[1]
+     var count = matches[2]
+     var total = price * count
+     tagOn('   ' + total + ' g')
+  -en
+ +trigger +n core.date-change
+  +p ^It is now the (.*) years after the fall of the Seleucarian Empire\.$
+  +do
+     highlight(matches[1],'orange')
+  -en
+`
+
+t.scripts = `
+`
+
+t.secondaries = `
+ sys
+`
+
+mods.register('core',t)
---
+++ user/ui/ui.main.js
@@ -0,0 +1,149 @@
+
+var t = {}
+t.scripts = `
+ +script
+  ui = typeof ui !== 'undefined' ? ui : {}
+  
+  ui.bg = 'fit'
+  ui.blur = 'blur(5px)'
+  ui.img = 'user/ui/resources/bg_dragonFire.jpg'
+  
+  ui.augment = function() {
+   var d = ''
+   // handle background
+   if (! $('#portrait').length) { 
+    d = '<div id="portrait">'
+    d += '</div>'
+    $('#container').append(d)
+   }
+   switch (ui.bg) {
+    case 'max':
+     $('#portrait').css({
+      'background-image': 'url("'+ui.img+'")',
+      'background-repeat': 'no-repeat',
+      'background-size': 'cover',
+      'position': 'absolute',
+      'left': '50%',
+      'top' : '50%',
+      'transform' : 'translate(-50%,-50%)',
+      'width'  : '100%',
+      'height' : '100%',
+      'filter' : ui.blur,
+      '-webkit-filter' : ui.blur,
+      '-moz-filter' : ui.blur,
+      '-o-filter' : ui.blur,
+      'z-index' : '-5',
+     })
+     break;
+    case 'fit':
+     $('#portrait').css({
+      'background-image': 'url("'+ui.img+'")',
+      'background-size': 'contain',
+      'background-repeat': 'no-repeat',
+      'background-position': 'center center',
+      'position': 'absolute',
+      'left': '50%',
+      'top' : '50%',
+      'width'  : '500px',
+      'height' : '360px',
+      'transform' : 'translate(-50%,-50%)',
+      'filter' : ui.blur,
+      '-webkit-filter' : ui.blur,
+      '-moz-filter' : ui.blur,
+      '-o-filter' : ui.blur,
+      'z-index' : '-5',
+     })
+    default:
+     $('#portrait').css({
+      'background-image': 'url("'+ui.img+'")',
+      'background-repeat': 'no-repeat',
+      'position': 'absolute',
+      'left': '50%',
+      'top' : '50%',
+      'transform' : 'translate(-50%,-50%)',
+      'width'  : '500px',
+      'height' : '360px',
+      'filter' : ui.blur,
+      '-webkit-filter' : ui.blur,
+      '-moz-filter' : ui.blur,
+      '-o-filter' : ui.blur,
+      'z-index' : '-5',
+     })
+     break;
+   }
+  }
+
+  ui.connect = function() {
+   $('#input').css('caret-color','rgba(45,155,45,1)')
+  }
+
+  ui.disconnect = function() {
+   $('#input').css('caret-color','rgba(145,45,45,1)')
+  }
+
+ -script
+`
+
+t.triggers = `
+`
+
+t.aliases = `
+ +alias
+  +p ^bg\\.max$
+  +do
+    ui.bg = 'max'
+    ui.augment()
+  -en
+ +alias
+  +p ^bg\\.fit$
+  +do
+    ui.bg = 'fit'
+    ui.augment()
+  -en
+ +alias
+  +p ^bg\\.std$
+  +p ^bg\\.min$
+  +do
+    ui.bg = 'std'
+    ui.augment()
+  -en
+ +alias
+  +p ^bg\\.img[ ]+(.*)$
+  +do
+    ui.img = 'user/ui/resources/'+matches[1]+'.jpg'
+    ui.augment()
+  -en
+ +alias
+  +p ^bg\\.blur[ ]+(.*)$
+  +do
+    ui.blur = 'blur('+matches[1]+'px)'
+    ui.augment()
+  -en
+`
+mods.register('ui',t)
+
+// https://i.pinimg.com/originals/1a/3b/0d/1a3b0d754e88d1a400c6695c2c44a3f4.png
+// https://i.pinimg.com/originals/a7/97/56/a79756280536f97921bcfd5154c39dc8.jpg
+// http://hddesktopwallpapers.in/wp-content/uploads/2015/06/Gold-Wallpapers-dragon.jpg
+// https://i.pinimg.com/originals/55/8e/b0/558eb0711833a04f1272c45be9e3904f.jpg
+// https://i.pinimg.com/originals/df/1d/63/df1d63eb28e47a11a879ea07d41182d4.jpg
+// https://i.pinimg.com/originals/ab/c2/48/abc248d7cdbee3e930d28d5933b56798.jpg
+// http://media.wizards.com/images/magic/daily/wallpapers/AVR_1_2560x1600_Wallpaper_s412m0imfi.jpg
+// https://images4.alphacoders.com/600/600921.jpg
+// http://www.nmgncp.com/data/out/177/5127214-hd-wallpapers-fantasy.jpg
+// http://www.guoguiyan.com/data/out/173/69696269-shadow-of-mordor-wallpapers.jpg
+// https://4.bp.blogspot.com/-xHMBFkU2-so/U1aOR6lR6VI/AAAAAAAA0No/zhCg2SlRyyg/s0/Diablo+III_3_Ultra+HD.jpg
+// http://media.blizzard.com/wow/media/wallpapers/other/blizzcon-2010/blizzcon-2010-1920x1080.jpg
+// http://wallpapercraft.net/wp-content/uploads/2016/11/Elf-Wallpapers-HD-For-Desktop.jpg
+// http://www.2kwp.com/wp-content/uploads/2016/11/Ship_005.jpg
+// https://digitalart.io/storage/artworks/227/Yellow-King-Skull-Girl-Wallpaper.jpeg
+// https://i.pinimg.com/originals/30/64/d9/3064d9f621add80aeab951cf007ebf82.jpg
+// http://elder-scrolls.com/uploads/posts/2013-11/1384926870_6.jpg
+// http://i.imgur.com/kybCF2F.jpg
+// http://digitalresult.com/wp-content/uploads/2015/08/skeleton-hd-wallpapers-31.jpeg
+// https://wallpaperscraft.com/image/mage_demon_magic_shield_attack_58211_1920x1200.jpg
+// https://www.artstation.com/artwork/zGmWm
+// https://www.artstation.com/artwork/xNKD2
+// https://cdna.artstation.com/p/assets/images/images/004/426/488/large/daryl-mandryk-nighthunters.jpg?1483661858
+// https://cdna.artstation.com/p/assets/images/images/001/224/252/large/dong-geon-son-15.jpg?1442507240
+// https://img00.deviantart.net/78b2/i/2015/313/5/1/demon_sage_by_jasontn-d9g4u3o.jpg
